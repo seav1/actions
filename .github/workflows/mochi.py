115d719e2f232d4bbb302a8926115bc8ef9d35f8
seav1 <1@seav.eu.org>
2026-07-18 11:15:00 +0800

    Create mochi.py

---
+++ .github/workflows/mochi.py
@@ -0,0 +1,29 @@
+name: Mochi Auto Renew
+
+on:
+  schedule:
+    - cron: '0 */4 * * *'
+  workflow_dispatch:
+
+jobs:
+  renew:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+        with:
+          repository: seav1/mochi
+          token: ${{ secrets.REPO_PAT }}
+          ref: main
+
+      - uses: alphaxcv/warp@1.0
+
+      - run: bash run.sh
+        env:
+          USERNAME: ${{ secrets.MOCHI_USERNAME }}
+          PASSWORD: ${{ secrets.MOCHI_PASSWORD }}
+
+      - if: failure()
+        uses: actions/upload-artifact@v4
+        with:
+          name: hidencloud-debug-screenshots
+          path: "*.png"
